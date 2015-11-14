05ee95ef6b5f107080b06586cdc008bfc7ee03ca
Francesco <capponi.francesco@gmail.com>
2015-11-13 21:59:37 -0800

    First Commit

---
+++ main.r
@@ -0,0 +1,35 @@
+
+price = 2
+initialMoney = 6
+nPuntate=2000
+
+times =1000
+wins = 0
+
+for(k in 1:times){
+  total = initialMoney
+  
+  for(t in 1:nPuntate){
+    total=total-price
+    if(total<0){
+      break
+    }
+    w=1
+    for(i in 1:100){
+      s = sample(1:2, 1, repl=F) 
+      if(s==1){
+        w=w*2
+      }else{
+        break;
+      }
+      total=total+w
+    }
+  }
+  if(total>initialMoney){
+    #print (total)
+    wins = wins +1
+  }
+}
+
+print(wins/times)
+
